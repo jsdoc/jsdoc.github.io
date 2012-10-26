4ef3812627e2b55aeb18edfca499af18e1b5e7e8
Matthew Kastor <matthewkastor@gmail.com>
2012-10-26 18:47:56 -0400

    Updated readme
    kastor.js was a jake file I used to sort lots of data without ruining my
    wrists and fingers. It is no longer necessary. articlePrep.bat is still
    useful because it will automatically regenerate the html files from
    jsdoc --describeTags and then rebuild the documentation project. I want
    to incorporate this action into the jake file in the future.

--- articlePrep.bat
+++ articlePrep.bat
@@ -1,3 +1,4 @@
+REM This file is used for regenerating the tags definition files. 
 set jsdocLoc=C:\Users\kastor\Documents\GitHub\jsdoc\
 set jsdocDocsLoc=C:\Users\kastor\Documents\GitHub\jsdoc3.github.com\
 
@@ -8,11 +9,6 @@ set describeTagsOutDir=%jsdocDocsLoc%%describeTagsLoc%
 
 REM generating API information about tags and putting files into the docs project
 call %jsdoc% --describeTags htmlFiles -d %describeTagsOutDir%
-
-
-REM pulling content out of jake articles and placing in extended docs
-REM cd "%jsdocDocsLoc%"
-REM call jake -f kastor
 
 REM rebuilding documentation project
 cd "%jsdocDocsLoc%"
